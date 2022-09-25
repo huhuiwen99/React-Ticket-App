bc3de8d3bddf97c078353b9bbbea06d61be81a81
huhuiwen <1050615501@qq.com>
2022-09-25 16:54:08 -0500

    add routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const express = require('express')
-const PORT = 5000
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
 
 const app = express()
+
+// create route
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
+    // res.send('Hello')
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
