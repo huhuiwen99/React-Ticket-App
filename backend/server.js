5ca74d9e83c00ec3a05f296b128f947abf227ecb
huhuiwen <1050615501@qq.com>
2022-09-25 17:42:17 -0500

    err handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // create route
 app.get('/', (req, res) => {
@@ -10,7 +14,9 @@ app.get('/', (req, res) => {
     // res.send('Hello')
 })
 
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
